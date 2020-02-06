f698dcca06ffaa12f2bb932cd0f843e85b6fd86c
SadWaifuGames <SadWaifuGames@gmail.com>
2020-02-05 18:38:29 -0700

    Level Menu Changes and Created Level1.1
    New fish and background sprites and lots more :)

---
+++ Fish Sticks/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+    public void GoToGameScene()
+    {
+        SceneManager.LoadScene("LevelMenu");
+    }
+}
