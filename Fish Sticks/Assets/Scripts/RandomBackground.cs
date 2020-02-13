a085b5ee959082bf9658e00e083e68ff48bbdcfd
SadWaifuGames <SadWaifuGames@gmail.com>
2020-02-12 22:55:22 -0700

    edited audio to be shorter

---
+++ Fish Sticks/Assets/Scripts/RandomBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBackground : MonoBehaviour
+{
+    //this script will choose a random background image for a scene
+    private int rand;
+    public Sprite[] backgroundSprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rand = Random.Range(0, backgroundSprites.Length);
+        GetComponent<SpriteRenderer>().sprite = backgroundSprites[rand];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
