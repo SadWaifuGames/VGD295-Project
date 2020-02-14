beb8994f4e21d4a8ec4f8c1c1bdc0e6bab5c026a
SadWaifuGames <SadWaifuGames@gmail.com>
2020-02-13 21:54:36 -0700

    added stars and score....
    still working on data persistence ;)

--- Fish Sticks/Assets/Scripts/GameMaster.cs
+++ Fish Sticks/Assets/Scripts/GameMaster.cs
@@ -2,15 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameMaster : MonoBehaviour
 {
     public GameObject loseScreen;
-      
+    public GameObject oneStar;
+    public GameObject twoStar;
+    public GameObject threeStar;
 
+
+    public Text score;
+    private bool hasLost;
+    public float playerScore;
+    string playerScoreString;
+    public int starCount;
+    public int levelCount;
+
+    void Awake ()
+    {
+        playerScore = 0.0f;
+        starCount = 0;
+        oneStar.SetActive(false);
+        twoStar.SetActive(false);
+        threeStar.SetActive(false);
+    }
+      
+    private void Update()
+    {
+        
+        if (hasLost == false)
+        {
+           playerScore = Time.time * 4f;
+           playerScoreString = playerScore.ToString("F0");
+           score.text = "Score:  " + playerScoreString;
+
+           if (playerScore >= 50f)
+            {
+                oneStar.SetActive(true);
+                starCount++;
+            }
+            if (playerScore >= 100f)
+            {
+                twoStar.SetActive(true);
+                starCount++;
+                levelCount++;
+            }
+            if (playerScore >= 150f)
+            {
+                threeStar.SetActive(true);
+                starCount++;
+            }
+        }
+
+        
+    }
 
     public void GameOver()
     {
+        hasLost = true;
         Invoke("Delay", 1.5f);
         
     }
---
+++ Fish Sticks/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class PlayerData 
+{
+    public int level;
+    public float score;
+    public int stars;
+
+    public PlayerData (GameMaster gameMaster)
+    {
+        level = gameMaster.levelCount;
+        score = gameMaster.playerScore;
+        stars = gameMaster.starCount;
+    }
+}
